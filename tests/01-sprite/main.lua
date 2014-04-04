3bac9f2d36c1d11070f37852498382e1d0e0dc98
gogoprog <gogoprog@gmail.com>
2014-04-04 13:31:08 +0200

    Added sprite test

---
+++ tests/01-sprite/main.lua
@@ -0,0 +1,31 @@
+function init()
+    application.setName("[gengine-tests] 01-sprite")
+    application.setExtent(320,200)
+end
+
+local logoEntity
+
+function start()
+    graphics.setClearColor(0,0.1,0.1,1)
+
+    graphics.texture.load("logo.png")
+
+    logoEntity = entity.create()
+
+    logoEntity:addComponent(
+        ComponentSprite(),
+        {
+            texture = graphics.texture.get("logo"),
+            extent = { x=256, y=128 }
+        }
+        )
+
+    logoEntity:insert()
+end
+
+local total = 0
+function update(dt)
+    total = total + dt
+    logoEntity.position.x = math.sin(total * 2) * 50
+end
+
