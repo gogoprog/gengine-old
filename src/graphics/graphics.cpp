08b0dafb8983a72bf163e25a2e4e24afb4bbb68e
gogoprog <gogoprog@gmail.com>
2014-04-05 17:18:00 +0200

    Scriptable graphics.createWorlds

--- src/graphics/graphics.cpp
+++ src/graphics/graphics.cpp
@@ -26,10 +26,18 @@ SCRIPT_FUNCTION(setClearColor)
     return 0;
 }
 
+SCRIPT_FUNCTION(createWorlds)
+{
+    System::getInstance().createWorlds(lua_tonumber(state, 1));
+
+    return 0;
+}
+
 SCRIPT_REGISTERER()
 {
     lua_newtable(state);
     SCRIPT_TABLE_PUSH_FUNCTION(setClearColor);
+    SCRIPT_TABLE_PUSH_FUNCTION(createWorlds);
 
     TextureManager::luaRegister(state);
     lua_setfield(state, -2, "texture");
