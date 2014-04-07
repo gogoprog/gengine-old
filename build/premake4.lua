a9255bd53b6db784d2649eb09028aebec4955605
gogoprog <gogoprog@gmail.com>
2014-04-07 14:13:46 +0200

    Updated premake

--- build/premake4.lua
+++ build/premake4.lua
@@ -20,7 +20,7 @@ solution "gengine"
         links { "lua" }
 
         flags { "ExtraWarnings", "FatalWarnings", "FloatFast", "NoExceptions", "NoFramePointer", "NoNativeWChar" }
-        buildoptions { "-std=c++11 -Wno-error=unused-variable" }
+        buildoptions { "-std=c++11 -Wno-error=unused-variable -Wno-error=unused-parameter" }
 
         configuration "Debug*"
             defines { "DEBUG" }
