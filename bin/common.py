2228a6e0e2c0c6160a41b2982ca77efc8445ddb4
gogoprog <gogoprog@gmail.com>
2014-04-22 21:57:16 +0200

    Added common python script

---
+++ bin/common.py
@@ -0,0 +1,38 @@
+#!/bin/python3
+
+import platform;
+import os;
+import sys;
+import argparse;
+
+debugMode = False
+targetDir = None
+
+def printn(*args):
+    sys.stdout.write(*args)
+
+def isPlatform64():
+    return "_64" in platform.machine()
+
+def sanityCheck():
+    printn("Sanity check... ")
+    if not "GENGINE" in os.environ:
+        print("GENGINE environment variable is not set.")
+        exit(1)
+    print("Ok!")
+
+def init():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-d', help='Debug mode', default=False, action='store_true')
+    parser.add_argument('dir', help='Target directory', default='.', nargs='?')
+    args = parser.parse_args()
+    debugMode = args.d
+    targetDir = os.getcwd() + "/" + args.dir + "/"
+
+def build(emscripten=False):
+    current_dir = os.getcwd()
+    config = ('debug' if debugMode else 'release') + ('emscripten' if emscripten else '') + ('64' if isPlatform64() else '32')
+    os.chdir(os.environ['GENGINE']+"/build")
+    os.system("premake4 gmake")
+    os.system(('emmake' if emscripten else '') + "make config=" + config)
+    os.chdir(current_dir)
