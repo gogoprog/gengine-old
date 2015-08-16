c98409e9d3790d69778de05324efead3de5fda46
Gauthier Billot <gauthier.billot@fishingcactus.com>
2015-08-16 12:05:35 +0200

    Wip on tiled support

--- src/tiled/tiled.cpp
+++ src/tiled/tiled.cpp
@@ -17,34 +17,87 @@ SCRIPT_REGISTERER()
             local path = filename:match("(.*/)")
             local file = dofile(filename)
             local indexToTileSet = {}
+            local indexToTile = {}
+
+            local function split(str, sep)
+               local result = {}
+               local regex = string.format("([^%s]+)", sep)
+               for line,_ in str:gmatch(regex) do
+                  table.insert(result, line)
+               end
+               return result
+            end
 
             if file then
                 for k, ts in ipairs(file.tilesets) do
                     local texture = gengine.graphics.texture.create(path .. ts.image)
                     ts.atlas = gengine.graphics.atlas.create(ts.name, texture, ts.imagewidth / ts.tilewidth,  ts.imageheight / ts.tileheight)
                     local count = (ts.imagewidth / ts.tilewidth) * (ts.imageheight / ts.tileheight)
+
                     for i=1,count do
                         table.insert(indexToTileSet, k)
+                        indexToTile[i + ts.firstgid - 1] = {properties={}, _Components={}}
+                    end
+
+                    for _, tile in ipairs(ts.tiles) do
+                        indexToTile[tile.id + ts.firstgid - 1] = tile
+                        if tile.properties.Component then
+                            tile._Components = split(tile.properties.Component, ",")
+                            tile.properties.Component = nil
+                        else
+                            tile._Components = {}
+                        end
                     end
                 end
 
                 for l, layer in ipairs(file.layers) do
+
+                    if layer.properties.Component then
+                        layer._Components = split(layer.properties.Component, ",")
+                        layer.properties.Component = nil
+                    else
+                        layer._Components = {}
+                    end
+
                     for k, v in ipairs(layer.data) do
                         if v ~= 0 then
                             local e = gengine.entity.create()
                             local x = (k-1) % layer.width
                             local y = layer.height - math.floor((k-1) / layer.width)
                             local ts = file.tilesets[indexToTileSet[v]]
+                            local tile = indexToTile[v]
+                            local properties = {}
+                            local components = {}
 
-                            e:addComponent(
-                                ComponentSprite(),
-                                {
-                                    atlas = gengine.graphics.atlas.get("crates"),
-                                    atlasItem = v - ts.firstgid,
-                                    extent = vector2(file.tilewidth, file.tileheight),
-                                    layer = l
-                                }
-                                )
+                            for _, p in ipairs(tile.properties) do table.insert(properties, p) end
+                            for _, p in ipairs(layer.properties) do table.insert(properties, p) end
+                            for _, c in ipairs(tile._Components) do table.insert(components, c) end
+                            for _, c in ipairs(layer._Components) do table.insert(components, c) end
+
+                            for _, component in ipairs(components) do
+
+                                if component == "Sprite" then
+                                    e:addComponent(
+                                        ComponentSprite(),
+                                        {
+                                            atlas = ts.atlas,
+                                            atlasItem = v - ts.firstgid,
+                                            extent = vector2(file.tilewidth, file.tileheight),
+                                            layer = l
+                                        }
+                                        )
+                                else
+                                    local constructor = _G["Component"..component]
+                                    if constructor then
+                                        e:addComponent(
+                                            constructor(),
+                                            properties
+                                            )
+                                    else
+                                        print("gengine.tiled : Unknown component " .. component)
+                                    end
+                                end
+                            end
 
                             e.position.x = x * file.tilewidth
                             e.position.y = y * file.tileheight
