c47d6ac90dfec025798d8f77f3e5884eda51dedb
Gauthier Billot <gauthier.billot@fishingcactus.com>
2015-08-26 14:45:19 +0200

    Workaround for SDL bug

--- src/application/application_window.cpp
+++ src/application/application_window.cpp
@@ -1,8 +1,11 @@
 #include "application_window.h"
 
 #include "core_sdl.h"
+#include "core.h"
 #include "application.h"
 #include <sstream>
+#include "debug.h"
+#include <GL/gl.h>
 
 #ifdef EMSCRIPTEN
     #include <emscripten.h>
@@ -32,10 +35,31 @@ void Window::init()
 
         if(application::isFullscreen())
         {
-            SDL_DisplayMode dm;
-            SDL_GetDesktopDisplayMode(0, &dm);
-            setExtent(dm.w, dm.h);
             flags |= SDL_WINDOW_FULLSCREEN_DESKTOP;
+
+            // workaround for SDL bug https://bugzilla.libsdl.org/show_bug.cgi?id=3105
+
+            SDL_DisplayMode dm;
+            SDL_Event e;
+
+            pWindow = SDL_CreateWindow("gengine", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 320, 200, SDL_WINDOW_OPENGL);
+            pRenderer = SDL_CreateRenderer(
+                pWindow,
+                -1,
+                SDL_RENDERER_ACCELERATED
+                );
+            context = SDL_GL_CreateContext(pWindow);
+
+            glClear(GL_COLOR_BUFFER_BIT);
+            while(SDL_PollEvent(&e));
+
+            int display_index = SDL_GetWindowDisplayIndex(pWindow);
+            SDL_GetDesktopDisplayMode(display_index, &dm);
+            setExtent(dm.w, dm.h);
+
+            SDL_GL_DeleteContext(context);
+            SDL_DestroyRenderer(pRenderer);
+            SDL_DestroyWindow(pWindow);
         }
 
         pWindow = SDL_CreateWindow(
