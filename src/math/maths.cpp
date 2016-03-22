bcd85acb6109328f6f90d3a91f39a352e9928c66
Gauthier Billot <gauthier.billot@fishingcactus.com>
2016-03-22 13:19:56 +0100

    Put back old math

--- src/math/maths.cpp
+++ src/math/maths.cpp
@@ -2,6 +2,7 @@
 
 #include "script.h"
 #include "vector4.h"
+#include "maths.h"
 
 #define PI 3.14159265358979323846264338327950288419716939937510f
 #define HALF_PI PI / 2
@@ -11,7 +12,7 @@ namespace gengine
 {
 namespace math
 {
-/*
+
 float getClosestAngle(const float angle, const float other_angle)
 {
     float lower_angle, upper_angle;
@@ -55,10 +56,10 @@ SCRIPT_FUNCTION(doRectanglesIntersect)
     bool
         result;
 
-    script::get(state, position1, 1);
-    script::get(state, half_extent1, 2);
-    script::get(state, position2, 3);
-    script::get(state, half_extent2, 4);
+    position1 = * script::get<Vector2>(state, 1);
+    half_extent1 = * script::get<Vector2>(state, 2);
+    position2 = * script::get<Vector2>(state, 3);
+    half_extent2 = * script::get<Vector2>(state, 4);
 
     half_extent1 *= 0.5f;
     half_extent2 *= 0.5f;
@@ -87,12 +88,12 @@ SCRIPT_FUNCTION(doCirclesIntersect)
     bool
         result;
 
-    script::get(state, position1, 1);
+    position1 = * script::get<Vector2>(state, 1);
     script::get(state, radius1, 2);
-    script::get(state, position2, 3);
+    position2 = * script::get<Vector2>(state, 3);
     script::get(state, radius2, 4);
 
-    //result = Vector2::getDistance(position1, position2) < radius1 + radius2;
+    result = (position1 - position2).Length() < radius1 + radius2;
 
     lua_pushboolean(state, result);
 
@@ -113,10 +114,10 @@ SCRIPT_FUNCTION(doesCircleIntersectRectangle)
     bool
         result;
 
-    script::get(state, circle_position, 1);
+    circle_position = * script::get<Vector2>(state,  1);
     radius = lua_tonumber(state, 2);
-    script::get(state, rect_position, 3);
-    script::get(state, half_extent, 4);
+    rect_position = * script::get<Vector2>(state,  3);
+    half_extent = * script::get<Vector2>(state,  4);
 
     half_extent *= 0.5f;
 
@@ -125,22 +126,22 @@ SCRIPT_FUNCTION(doesCircleIntersectRectangle)
 
     square_distance = 0.0f;
 
-    if(circle_position.x < minimum_point.x)
+    if(circle_position.x_ < minimum_point.x_)
     {
-        square_distance += (circle_position.x - minimum_point.x) * (circle_position.x - minimum_point.x);
+        square_distance += (circle_position.x_ - minimum_point.x_) * (circle_position.x_ - minimum_point.x_);
     }
-    else if(circle_position.x > maximum_point.x)
+    else if(circle_position.x_ > maximum_point.x_)
     {
-        square_distance += (circle_position.x - maximum_point.x) * (circle_position.x - maximum_point.x);
+        square_distance += (circle_position.x_ - maximum_point.x_) * (circle_position.x_ - maximum_point.x_);
     }
 
-    if(circle_position.y < minimum_point.y)
+    if(circle_position.y_ < minimum_point.y_)
     {
-        square_distance += (circle_position.y - minimum_point.y) * (circle_position.y - minimum_point.y);
+        square_distance += (circle_position.y_ - minimum_point.y_) * (circle_position.y_ - minimum_point.y_);
     }
-    else if(circle_position.y > maximum_point.y)
+    else if(circle_position.y_ > maximum_point.y_)
     {
-        square_distance += (circle_position.y - maximum_point.y) * (circle_position.y - maximum_point.y);
+        square_distance += (circle_position.y_ - maximum_point.y_) * (circle_position.y_ - maximum_point.y_);
     }
 
     result = square_distance < radius * radius;
@@ -149,18 +150,17 @@ SCRIPT_FUNCTION(doesCircleIntersectRectangle)
 
     return 1;
 }
-*/
+
 SCRIPT_REGISTERER()
 {
     lua_newtable(state);
 
-    //Vector2::luaRegister(state);
     Vector4::luaRegister(state);
 
-    /*SCRIPT_TABLE_PUSH_FUNCTION(getClosestAngle);
+    SCRIPT_TABLE_PUSH_FUNCTION(getClosestAngle);
     SCRIPT_TABLE_PUSH_FUNCTION(doRectanglesIntersect);
     SCRIPT_TABLE_PUSH_FUNCTION(doCirclesIntersect);
-    SCRIPT_TABLE_PUSH_FUNCTION(doesCircleIntersectRectangle);*/
+    SCRIPT_TABLE_PUSH_FUNCTION(doesCircleIntersectRectangle);
 
     lua_setfield(state, -2, "math");
 }
